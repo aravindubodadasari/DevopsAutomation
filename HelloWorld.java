294658c228455284584f245e48e098897a99f15a
aravindu <aravindb402@gmail.com>
2025-08-06 14:12:39 +0530

    task done

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/aravindubodadasari/DevopsAutomation.git']])
